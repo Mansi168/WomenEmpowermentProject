a9c88d6cc9ba640bee038052533ecee6a31788af
Mansi <mansins168@gmail.com>
2023-02-11 20:56:55 +0530

    Changes updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -148,7 +148,7 @@ const Header = () => {
                   <div className="d-flex align-items-center gap-50 text-dark">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/openStore">Open Store</NavLink>
-                    <NavLink to="/product">My Store</NavLink>
+                    <NavLink to="/product">Explore Products</NavLink>
                     <NavLink to="/blogs">Our stories</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
                   </div>
